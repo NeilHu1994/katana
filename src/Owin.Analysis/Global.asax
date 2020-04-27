d6ec1d3c1b5d234eff02ea02c811d2df8a2da6cd
Fei HU <812499@cognizant.com>
2020-04-27 16:28:48 +0800

    add owin analysis for katana

---
+++ src/Owin.Analysis/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Owin.Analysis.MvcApplication" Language="C#" %>
---
+++ src/Owin.Analysis/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Owin.Analysis.Startup))]
+
+namespace Owin.Analysis
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use((context, next) =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "Middleware 1");
+                return next.Invoke();
+            });
+            app.Use((context, next) =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "2nd MW");
+                return next.Invoke();
+            });
+            //app.UseStageMarker(PipelineStage.Authenticate);
+            app.Run(context =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "3rd MW");
+                return context.Response.WriteAsync("Hello world");
+            });
+            //app.UseStageMarker(PipelineStage.ResolveCache);
+        }
+
+        private void PrintCurrentIntegratedPipelineStage(IOwinContext context, string msg)
+        {
+            var currentIntegratedpipelineStage = HttpContext.Current.CurrentNotification;
+            context.Get<TextWriter>("host.TraceOutput").WriteLine(
+                "Current IIS event: " + currentIntegratedpipelineStage
+                + " Msg: " + msg);
+        }
+    }
+}
